c51a76878169e3ff37cc7a5dc418e2c39937a571
lexcomesdev <57563562+lexcomesdev@users.noreply.github.com>
2020-02-07 10:42:11 +0100

    added modules map for extra modules

--- V1OOP Lex Comes/src/practicums/practicum5/Klas.java
+++ V1OOP Lex Comes/src/practicums/practicum5/Klas.java
@@ -1,4 +1,41 @@
 package practicums.practicum5;
 
+import java.util.ArrayList;
+
 public class Klas {
+    private String klasCode;
+    private ArrayList<Leerling> leerlingen;
+
+    public Klas(String kC){
+        klasCode = kC;
+        leerlingen = new ArrayList<Leerling>();
+    }
+
+    public void voegLeerlingToe(Leerling ll){
+        leerlingen.add(ll);
+    }
+
+    public void wijzigCijfer(String nm, double nweCijfer){
+        for (Leerling leerling : leerlingen){
+            if (nm == leerling.getNaam()){
+                leerling.setCijfer(nweCijfer);
+            }
+        }
+    }
+
+    public ArrayList<Leerling> getLeerlingen() {
+        return leerlingen;
+    }
+    public int aantalLeerlingen(){
+        return leerlingen.size();
+    }
+
+    public String toString() {
+        String s = "In klas " + klasCode + " zitten de volgende leerlingen: \n";
+        for (Leerling leerling : leerlingen){
+            String s2 = leerling.getNaam() + " heeft cijfer: " + leerling.getCijfer() + "\n";
+            s = s + s2;
+        }
+        return s;
+    }
 }
--- V1OOP Lex Comes/src/practicums/practicum5/Leerling.java
+++ V1OOP Lex Comes/src/practicums/practicum5/Leerling.java
@@ -1,4 +1,30 @@
 package practicums.practicum5;
 
 public class Leerling {
+    private String naam;
+    private double cijfer = 0;
+
+    public Leerling(String nm){
+        naam = nm;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double getCijfer() {
+        return cijfer;
+    }
+
+    public void setCijfer(double cijfer) {
+        this.cijfer = cijfer;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String toString() {
+        return naam;
+    }
 }
